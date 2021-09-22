dce67d502a9c2227b5dc89ae62d5255270bcdf33
Manuel Francisco Alonso <manuelalonso89@gmail.com>
2021-09-21 22:17:25 -0300

    Login fragment and layout created. Base folders created

--- app/src/main/java/com/bifrost/cocinarte/activities/MainActivity.kt
+++ app/src/main/java/com/bifrost/cocinarte/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bifrost.cocinarte
+package com.bifrost.cocinarte.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bifrost.cocinarte.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/bifrost/cocinarte/models/LogInViewModel.kt
@@ -0,0 +1,7 @@
+package com.bifrost.cocinarte.models
+
+import androidx.lifecycle.ViewModel
+
+class LogInViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
