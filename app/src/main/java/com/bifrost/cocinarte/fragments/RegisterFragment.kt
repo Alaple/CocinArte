e43c2665e0c87b432609a02e151a288f5d9b6eb7
marianofidalgo <marianofidalgo@gmail.com>
2021-09-30 00:47:46 -0300

    Pantallas Register y Started

---
+++ app/src/main/java/com/bifrost/cocinarte/fragments/RegisterFragment.kt
@@ -0,0 +1,105 @@
+package com.bifrost.cocinarte.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.bifrost.cocinarte.R
+import com.bifrost.cocinarte.models.RegisterViewModel
+import com.google.android.material.snackbar.Snackbar
+
+class RegisterFragment : Fragment() {
+
+    lateinit var v: View
+
+    // Component variables
+    lateinit var txtRegister: TextView
+    lateinit var txtFullName: TextView
+    lateinit var inputFullName: EditText
+    lateinit var txtEmail: TextView
+    lateinit var inputEmail: EditText
+    lateinit var txtPassword: TextView
+    lateinit var inputPassword: EditText
+    lateinit var btnRegister: Button
+    lateinit var txtLogIn: TextView
+
+    // For snackbar use
+    lateinit var rootLayout: ConstraintLayout
+
+    companion object {
+        fun newInstance() = RegisterFragment()
+    }
+
+    private lateinit var viewModel: RegisterViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        v = inflater.inflate(R.layout.register_fragment, container, false)
+
+        // Initialize variables
+        txtRegister = v.findViewById(R.id.textRegister)
+        txtFullName = v.findViewById(R.id.textFullName)
+        inputFullName = v.findViewById(R.id.inputFullName)
+        txtEmail = v.findViewById(R.id.textEmail)
+        inputEmail = v.findViewById(R.id.inputEmail)
+        txtPassword = v.findViewById(R.id.textPassword)
+        inputPassword = v.findViewById(R.id.inputPassword)
+        btnRegister = v.findViewById(R.id.buttonRegister)
+        txtLogIn = v.findViewById(R.id.textLogIn)
+        // For snackbar use
+        rootLayout = v.findViewById(R.id.loginLayout)
+
+        return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Initialize all text variables
+        initializeText()
+
+        // Initialize all buttons variables
+        initializeButtons()
+    }
+
+    private fun initializeText() {
+        txtRegister.setText("REGISTER")
+        txtFullName.setText("FULL NAME")
+        inputFullName.setHint("Jhon Doe")
+        txtEmail.setText("EMAIL")
+        inputEmail.setHint("jhondoe@email.com")
+        txtPassword.setText("PASSWORD")
+        inputPassword.setHint("Password")
+        btnRegister.setText("REGISTER")
+        txtLogIn.setText("ALREADY HAVE AN ACCOUNT? LOGIN")
+    }
+
+    private fun initializeButtons() {
+        // REGISTER button
+        btnRegister.setOnClickListener() {
+            // TODO Register
+            Snackbar.make(rootLayout, "TODO REGISTER", Snackbar.LENGTH_SHORT).show()
+        }
+
+        // LOGIN button
+        txtLogIn.setOnClickListener() {
+            // TODO LogIn
+            Snackbar.make(rootLayout, "TODO LOGIN", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}
---
+++ app/src/main/java/com/bifrost/cocinarte/fragments/StartedFragment.kt
@@ -0,0 +1,102 @@
+package com.bifrost.cocinarte.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.bifrost.cocinarte.R
+import com.bifrost.cocinarte.models.LogInViewModel
+import com.bifrost.cocinarte.models.StartedViewModel
+import com.google.android.material.snackbar.Snackbar
+
+class StartedFragment : Fragment() {
+
+    lateinit var v: View
+
+    // Component variables
+    lateinit var txtStarted: TextView
+    lateinit var msgStarted: TextView
+    lateinit var btnLogin: Button
+    lateinit var btnRegister: Button
+    lateinit var txtSkip: TextView
+
+    // For snackbar use
+    lateinit var rootLayout: ConstraintLayout
+
+    companion object {
+        fun newInstance() = StartedFragment()
+    }
+
+    private lateinit var viewModel: StartedViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        v = inflater.inflate(R.layout.started_fragment, container, false)
+
+        // Initialize variables
+        txtStarted = v.findViewById(R.id.textStarted)
+        msgStarted = v.findViewById(R.id.messageStarted)
+        btnLogin = v.findViewById(R.id.buttonLogIn)
+        btnRegister = v.findViewById(R.id.buttonRegister)
+        txtSkip = v.findViewById(R.id.textSkip)
+        // For snackbar use
+        rootLayout = v.findViewById(R.id.loginLayout)
+
+        return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Initialize all text variables
+        initializeText()
+
+        // Initialize all buttons variables
+        initializeButtons()
+    }
+
+    private fun initializeText() {
+        txtStarted.setText("GET STARTED!")
+        msgStarted.setText("Get started and enjoy the awesome food recepies that we have for you!")
+        btnLogin.setText("LOGIN")
+        btnRegister.setText("REGISTER")
+        txtSkip.setText("SKIP THIS PART")
+    }
+
+    private fun initializeButtons() {
+        // LOGIN button
+        btnLogin.setOnClickListener() {
+            // TODO Login
+            Snackbar.make(rootLayout, "TODO LOGIN", Snackbar.LENGTH_SHORT).show()
+        }
+
+        // REGISTER button
+        btnRegister.setOnClickListener() {
+            // TODO Register
+            Snackbar.make(rootLayout, "TODO REGISTER", Snackbar.LENGTH_SHORT).show()
+        }
+
+        // Forgot Password text button
+        txtSkip.setOnClickListener() {
+            // TODO Skip
+            Snackbar.make(rootLayout, "TODO SKIP LINK", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(StartedViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
