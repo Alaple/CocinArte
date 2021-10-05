4f4220bad16a16782ab445c14e7329ccd422e74b
Manuel Francisco Alonso <manuelalonso89@gmail.com>
2021-10-05 00:28:18 -0300

    gitignore v2

---
+++ app/src/main/java/com/bifrost/cocinarte/fragments/AccountProfileFragment.kt
@@ -0,0 +1,33 @@
+package com.bifrost.cocinarte.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bifrost.cocinarte.R
+import com.bifrost.cocinarte.models.AccountProfileViewModel
+
+class AccountProfileFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AccountProfileFragment()
+    }
+
+    private lateinit var viewModel: AccountProfileViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.account_profile_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AccountProfileViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
