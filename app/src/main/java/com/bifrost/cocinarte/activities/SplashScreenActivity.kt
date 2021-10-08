f9144b449def88e64c1e808cbec8bc3bec0a0ef9
shape-shifters <76792635+shape-shifters@users.noreply.github.com>
2021-10-08 19:02:52 -0300

    SplashScreen (#11)

---
+++ app/src/main/java/com/bifrost/cocinarte/activities/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.bifrost.cocinarte.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.bifrost.cocinarte.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        handler = Handler()
+        handler.postDelayed({
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
