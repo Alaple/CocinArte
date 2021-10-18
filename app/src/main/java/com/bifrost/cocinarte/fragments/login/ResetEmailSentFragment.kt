b4497b775411456175dd0b041a02bcee5f4f6541
Alaple <lucaspalac94@hotmail.com>
2021-10-18 19:08:03 -0300

    Added reset email sent screen

---
+++ app/src/main/java/com/bifrost/cocinarte/fragments/login/ResetEmailSentFragment.kt
@@ -0,0 +1,55 @@
+package com.bifrost.cocinarte.fragments.login
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import com.bifrost.cocinarte.R
+import com.bifrost.cocinarte.entities.Login
+import com.bifrost.cocinarte.models.login.ResetEmailSentViewModel
+
+class ResetEmailSentFragment : Fragment() {
+
+    lateinit var v: View
+
+    lateinit var btnBackToLogin: Button
+
+    companion object {
+        fun newInstance() = ResetEmailSentFragment()
+    }
+
+    private lateinit var viewModel: ResetEmailSentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        v = inflater.inflate(R.layout.reset_email_sent_fragment, container, false)
+
+        btnBackToLogin = v.findViewById(R.id.btnBackToLogin)
+
+        return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        btnBackToLogin.setOnClickListener() {
+            // TODO Delete account
+            //val action = ResetEmailSentFragmentDirections.actionStartedFragmentToLogInFragment();
+            ///v.findNavController().navigate(action)
+        }
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ResetEmailSentViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/bifrost/cocinarte/models/login/ResetEmailSentViewModel.kt
@@ -0,0 +1,7 @@
+package com.bifrost.cocinarte.models.login
+
+import androidx.lifecycle.ViewModel
+
+class ResetEmailSentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
