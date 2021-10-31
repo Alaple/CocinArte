a949c8685781774190605bf2fa252d05104fc4a8
Alaple <lucaspalac94@hotmail.com>
2021-10-31 16:13:14 -0300

    Add updated Home screen with recipes list calling api

--- app/src/main/java/com/bifrost/cocinarte/entities/ApiCaller.kt
+++ app/src/main/java/com/bifrost/cocinarte/entities/ApiCaller.kt
@@ -2,9 +2,7 @@ package com.bifrost.cocinarte.entities
 
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
-import retrofit2.http.Url
 
 interface ApiCaller {
     @GET("v2")
