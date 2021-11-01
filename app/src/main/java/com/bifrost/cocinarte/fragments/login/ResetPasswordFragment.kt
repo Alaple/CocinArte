8bf9ae5bd068f62b8d9e692221234cc66543419a
marianofidalgo <62575541+marianofidalgo@users.noreply.github.com>
2021-10-31 21:14:49 -0300

    Se trabajo para que la pantalla de ResetPassword envie un email al usuario usando Firebase. (#26)
    Co-authored-by: Exequiel Fidalgo <exequielfidalgo@gmail.com>

--- app/src/main/java/com/bifrost/cocinarte/fragments/login/ResetPasswordFragment.kt
+++ app/src/main/java/com/bifrost/cocinarte/fragments/login/ResetPasswordFragment.kt
@@ -2,20 +2,32 @@ package com.bifrost.cocinarte.fragments.login
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.findNavController
 import com.bifrost.cocinarte.R
 import com.bifrost.cocinarte.models.login.ResetPasswordViewModel
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class ResetPasswordFragment : Fragment() {
 
     lateinit var v: View
 
+    lateinit var rootLayout: ConstraintLayout
+
+    lateinit var inputResetEmail: TextView
     lateinit var btnResetPassword: Button
+
+     private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
 
     companion object {
         fun newInstance() = ResetPasswordFragment()
@@ -29,7 +41,13 @@ class ResetPasswordFragment : Fragment() {
     ): View? {
         v = inflater.inflate(R.layout.reset_password_fragment, container, false)
 
+        rootLayout = v.findViewById(R.id.ResetPasswordConstraintLayout)
+
+        inputResetEmail = v.findViewById(R.id.inputResetEmail)
         btnResetPassword = v.findViewById(R.id.btnResetPassword)
+
+        database= FirebaseDatabase.getInstance()
+        auth = FirebaseAuth.getInstance()
 
         return v
     }
@@ -38,9 +56,21 @@ class ResetPasswordFragment : Fragment() {
         super.onStart()
 
         btnResetPassword.setOnClickListener() {
-            // TODO Reset password
-            val action = ResetPasswordFragmentDirections.actionResetPasswordFragmentToResetEmailSentFragment2()
-            v.findNavController().navigate(action)
+            val email: String = inputResetEmail.text.toString()
+
+            if(email.isNotEmpty()) {
+                auth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val action = ResetPasswordFragmentDirections.actionResetPasswordFragmentToResetEmailSentFragment2()
+                            v.findNavController().navigate(action)
+                        } else {
+                            Snackbar.make(rootLayout, "The e-mail is incorrect.", Snackbar.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Snackbar.make(rootLayout, "The e-mail cannot be empty.", Snackbar.LENGTH_SHORT).show()
+            }
         }
     }
 
