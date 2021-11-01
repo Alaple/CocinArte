013ad4062f3b713ddbbe40909043d6486309184d
unknown <alvarezkusy.julian@gmail.com>
2021-11-01 14:48:06 -0300

    COC-113-RenderRecipes: Modify filter name & loadButtons func renamed

--- app/src/main/java/com/bifrost/cocinarte/fragments/main/ListIngredients.kt
+++ app/src/main/java/com/bifrost/cocinarte/fragments/main/ListIngredients.kt
@@ -1,7 +1,6 @@
 package com.bifrost.cocinarte.fragments.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,8 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bifrost.cocinarte.R
 import com.bifrost.cocinarte.adapters.ButtonListAdapter
 import com.bifrost.cocinarte.adapters.RecipesListAdapter
-import com.bifrost.cocinarte.entities.RecipesDataCollectionItem
-import com.bifrost.cocinarte.models.main.ListIngredientsButtonsViewModel
+import com.bifrost.cocinarte.models.main.ListIngredientsViewModel
 import com.google.android.material.textfield.TextInputEditText
 
 class ListIngredients : Fragment() {
@@ -30,7 +28,7 @@ class ListIngredients : Fragment() {
     private lateinit var linearLayoutManager: LinearLayoutManager
     private lateinit var buttonListAdapter: ButtonListAdapter
     private lateinit var recipesListAdapter: RecipesListAdapter
-    private lateinit var buttonsViewModel: ListIngredientsButtonsViewModel
+    private lateinit var buttonsViewModel: ListIngredientsViewModel
     private lateinit var filterArgList: MutableList<String>
     lateinit var v: View
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,9 +45,9 @@ class ListIngredients : Fragment() {
         buttons = v.findViewById(R.id.buttonsRecView)
         recipesRecView = v.findViewById(R.id.recipesRecView)
         searchButton = v.findViewById(R.id.searchBarButton)
-        buttonsViewModel = ViewModelProvider(requireActivity()).get(ListIngredientsButtonsViewModel::class.java)
+        buttonsViewModel = ViewModelProvider(requireActivity()).get(ListIngredientsViewModel::class.java)
         filterArgList = ArrayList()
-        buttonsViewModel.cargarTest()
+        buttonsViewModel.loadButtons()
 
 
 
