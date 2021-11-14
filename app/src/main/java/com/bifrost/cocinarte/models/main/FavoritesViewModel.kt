1b2b720493da836f4af5532e79a5588cfd701b12
Exequiel Fidalgo <exequielfidalgo@gmail.com>
2021-11-14 13:08:14 -0300

    Se agrego en el detalle de la receta la opción para agregar como favorito. Se agregan los favoritos a la db pero no se controlan los duplicados o se eliminan los mismos. Además la pantalla de favoritos esta listando correctamente pero no es posible acceder a la receta.

---
+++ app/src/main/java/com/bifrost/cocinarte/models/main/FavoritesViewModel.kt
@@ -0,0 +1,50 @@
+package com.bifrost.cocinarte.models.main
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.bifrost.cocinarte.entities.RecipeHit
+import com.bifrost.cocinarte.entities.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+
+class FavoritesViewModel : ViewModel() {
+
+    private val db = Firebase.firestore
+    private lateinit var auth: FirebaseAuth
+
+    var favorites: MutableList<RecipeHit> = mutableListOf()
+    var favoritesLiveData: MutableLiveData<MutableList<RecipeHit>> = MutableLiveData()
+
+    fun initializeProfile() {
+        getUserDB()
+    }
+
+    private fun getUserDB() {
+        auth = FirebaseAuth.getInstance()
+        val authUser = auth.currentUser
+        if (authUser != null) {
+            getUserFirestore(authUser.email)
+        }
+    }
+
+    private fun getUserFirestore(email: String?) {
+        val tag = "FavoriteViewModel - getUser"
+        val docRef = db.collection("users").document(email!!)
+        docRef.get()
+            .addOnSuccessListener { dataSnapshot ->
+                if (dataSnapshot != null) {
+                    var user = dataSnapshot.toObject<User>()!!
+                    favorites = user.favorite!!
+                    favoritesLiveData.value = user.favorite
+                } else {
+                    Log.d(tag, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(tag, "get failed with ", exception)
+            }
+    }
+}
